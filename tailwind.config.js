b2d0d5dba57b7b356df9ab7f8ea06be4368b9e96
DavidLucas1122 <DavidLucas1122>
2025-06-14 17:20:54 -0300

    Medidas Tablet Corrigidas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+  content: {
+    files: ['./**/*.{html,js}'], // ou o caminho correto do seu projeto
+    breakpoints: true,           // 👈 ativa o uso de max-[breakpoint]
+  },
   theme: {
     extend: {},
     screens: {
-      'xs': '380px', // novo breakpoint
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
   },
+  plugins: [],
 }
